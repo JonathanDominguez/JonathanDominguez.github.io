b96f1e76ef4a775f57adc6bbb05f938da4e11b4f
Jonathan Dominguez <DominguezE.Jonathan@gmail.com>
2021-05-06 16:28:43 -0700

    index completed and responsive

--- scripts/index.js
+++ scripts/index.js
@@ -5,3 +5,12 @@ function moveDown(){
         behavior: 'smooth'
     });
 }
+
+function myFunction() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  }
